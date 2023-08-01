f5e98f7b5a60ecd2ba2c8fe49f856b9265ef86d6
Joel <joellongaresjr@gmail.com>
2023-08-01 16:37:06 -0700

    added notes to my code and fixed bug within our for loop in delete route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require("express"); 
 const path = require("path");
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
-const PORT = process.env.PORT || 3001;
+const { v4: uuidv4 } = require("uuid"); // using uuid to generate unique id
+const PORT = process.env.PORT || 3001; //
 
 const app = express();
 
@@ -11,52 +11,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+// Route routtteeeee (jerome voice)
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
-
+// Route for "/notes" sending notes to "public/notes.html"
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+// api route to get (fetch) all notes from db.json
 app.get("/api/notes", (req, res) => {
   fs.readFile("db/db.json", "utf-8", (err, data) => {
     err ? console.log(err) : res.json(JSON.parse(data));
   });
 });
 
-// POST /api/notes
+// Route for Post request for adding new notes
 app.post("/api/notes", (req, res) => {
-  let title = req.body.title;
-  let text = req.body.text;
-  let newNote = { title, text, id: uuidv4() };
+  let title = req.body.title; // extracting note title from request body 
+  let text = req.body.text; // extracting note text from request body
+  let newNote = { title, text, id: uuidv4() }; // applying uuid npm package to give each note a unique id
 
-  // reading the file
+  // reading the file in db.json
   fs.readFile("db/db.json", "utf-8", (err, data) => {
     if (err) {
       console.log("Error reading db.json:", err);
       res.status(500).json({ err: "Failed to read notes from database." });
       return;
     }
-    let currentNotes = JSON.parse(data); //json parse existing
+    let currentNotes = JSON.parse(data); //json parse existing data into an array 
     currentNotes.push(newNote); // add new notes (push)
     fs.writeFile("db/db.json", JSON.stringify(currentNotes, null, 4), (err) => {
-      //turnary
+      //turnary: shoutout my tutor for this way of code making it look cleaner/pretter! definetly gonna be using this more often
+      //writing updated array back to our json file (added null,4) to organize our db.json to look more readable to acquire id
       err
         ? console.log("Failed to write note to database", err)
         : console.log("New Note has been saved!");
     });
 
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html")); // sending our responses to notes.html
   });
-  // parsing the file into an array
-  // pushing new note to array
-  // write file
 });
 
 app.delete("/api/notes/:id", (req, res) => {
   // creating a new route through notes with our ':id' parameters of our db.json
-  const deleteId = req.params.id; // extracting Id parameter assigning it variable 'deleteId'
+  const deleteId = req.params.id; // extracting Id parameter assigning it variable 'deleteId' , 
   fs.readFile("db/db.json", "utf-8", (err, data) => {
     if (err) {
       console.log("Error reading db.json", err);
@@ -66,7 +66,7 @@ app.delete("/api/notes/:id", (req, res) => {
       return;
     }
 
-    let currentNotes = JSON.parse(data);
+    let currentNotes = JSON.parse(data); // parasing the existing db.json data into an array
 
     for (let i = 0; i < currentNotes.length; i++) {
       // loop through array to identifying the specific ID
@@ -86,6 +86,7 @@ app.delete("/api/notes/:id", (req, res) => {
   });
 });
 
+//catching wild card routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
@@ -93,3 +94,4 @@ app.get("*", (req, res) => {
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} `)
 );
+
